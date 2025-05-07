8a522cad3b60b70034856aed9dd88c1ab1e5f9b0
Guilherme De Meira Nazar <guilhermen610@gmail.com>
2025-05-07 15:58:52 -0300

    Usando classe

---
+++ ex02/UsandoClasse/Bytebank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 361;
+            contaDaGabriela.numero = 98676;
+            contaDaGabriela.saldo = 500.50;
+
+            Console.WriteLine("Titular: " + contaDaGabriela.titular);
+            Console.WriteLine("Agencia: " + contaDaGabriela.agencia);
+            Console.WriteLine("Numero: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+          
+            Console.ReadLine();
+        }
+    }
+}
