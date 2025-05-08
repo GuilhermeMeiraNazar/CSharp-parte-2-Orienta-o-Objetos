b0aafa1db56c3f1821c22e3c51806f94ddcc3b39
Guilherme De Meira Nazar <guilhermen610@gmail.com>
2025-05-08 11:55:05 -0300

    Comportamento de classe e funçao

---
+++ ex03/ComportamentoClasses/Funcao/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.titular = "Bruno";
+            contaDoBruno.saldo = 1000;
+
+            Console.WriteLine(contaDoBruno.saldo);
+            contaDoBruno.Sacar(50);
+            Console.WriteLine(contaDoBruno.saldo);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
