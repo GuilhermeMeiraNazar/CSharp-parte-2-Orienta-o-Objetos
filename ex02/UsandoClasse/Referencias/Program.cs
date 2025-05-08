70beaed832b55070d73bd70275cf7baf9f4ce2bd
Guilherme De Meira Nazar <guilhermen610@gmail.com>
2025-05-08 10:58:14 -0300

    referencia / Valores padrao

---
+++ ex02/UsandoClasse/Referencias/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 84646;
+
+            ContaCorrente contaDaGabrielaCosta = new ContaCorrente();
+            contaDaGabrielaCosta.titular = "Gabriela";
+            contaDaGabrielaCosta.agencia = 863;
+            contaDaGabrielaCosta.numero = 84646;
+
+
+
+        }
+    }
+}
---
+++ ex02/UsandoClasse/ValoresPadroes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente conta = new ContaCorrente();
+            conta.titular = "Gabriela";
+            Console.WriteLine(conta.titular);
+            Console.ReadLine();
+
+        }
+    }
+}
