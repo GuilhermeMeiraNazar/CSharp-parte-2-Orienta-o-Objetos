a64754ed197bff161b9d24856aa401164266afd0
Guilherme De Meira Nazar <guilhermen610@gmail.com>
2025-05-14 10:20:59 -0300

    Namespace

---
+++ ex04/Namespace_composicao_e_null/namespaces/ContaCorrente.cs
@@ -0,0 +1,33 @@
+﻿//using namespaces;
+
+namespace namespaces
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        public double saldo;
+
+        public bool Sacar(double valor)
+        {
+            if (this.saldo < valor)
+            // this é usando para refrerencia que é desse objeto
+            {
+                return false;
+                // retorna o valor da função
+            }
+            else
+            {
+                this.saldo -= 100;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+    }
+}
+
---
+++ ex04/Namespace_composicao_e_null/namespaces/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace namespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente gabriela = new Cliente();
+            gabriela.nome = "Gabriela";
+            gabriela.profissao = "Desenvolvedora";
+            gabriela.cpf = "559985.65752.9595";
+
+            ContaCorrente conts = new ContaCorrente();
+
+        }
+    }
+}
