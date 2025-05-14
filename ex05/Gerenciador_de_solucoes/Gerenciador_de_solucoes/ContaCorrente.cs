bd8a54e8705f533dff767245a148dae4673880e1
Guilherme De Meira Nazar <guilhermen610@gmail.com>
2025-05-14 11:37:23 -0300

    Gerenciador de soluções / Get and Set

---
+++ ex05/Gerenciador_de_solucoes/Gerenciador_de_solucoes/ContaCorrente.cs
@@ -0,0 +1,71 @@
+﻿//using namespaces;
+
+using Gerenciador_de_solucoes;
+
+namespace Gerenciador_de_solucoes
+{
+    public class ContaCorrente
+    {
+        //private Cliente _titular;
+
+        //public Cliente Titular
+        //{
+        //    get 
+        //    {
+        //        return _titular; 
+        //    }
+        //    set
+        //    {
+        //        _titular = value;
+        //    }
+        //}
+
+        public Cliente Titular{get;set;}
+        public int Agencia {get;set;}
+        public int Numero { get;set;}
+
+
+        private double _saldo;
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                else
+                {
+                    _saldo = value;
+                }
+            }
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (this._saldo < valor)
+            // this é usando para refrerencia que é desse objeto
+            {
+                return false;
+                // retorna o valor da função
+            }
+            else
+            {
+                this._saldo -= 100;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+    }
+}
+
---
+++ ex05/Gerenciador_de_solucoes/Gerenciador_de_solucoes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gerenciador_de_solucoes;
+
+namespace Gerenciador_de_solucoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            conta.Saldo = 100;
+            Console.Write(conta.Saldo);
+
+            Cliente cliente = new Cliente();
+            cliente.Nome = "Guilherme";
+            cliente.CPF = "8464656";
+            cliente.Profissao = "Desenvolvedor";
+
+            Console.ReadLine();
+
+        }
+    }
+}
