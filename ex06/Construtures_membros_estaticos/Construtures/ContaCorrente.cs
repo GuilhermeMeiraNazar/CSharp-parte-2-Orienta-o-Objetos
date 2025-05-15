e9b0dfc4d4a7c65e93b1b70db75142dca9b1b673
Guilherme De Meira Nazar <guilhermen610@gmail.com>
2025-05-15 11:45:32 -0300

    Construtores

---
+++ ex06/Construtures_membros_estaticos/Construtures/ContaCorrente.cs
@@ -0,0 +1,94 @@
+﻿//using namespaces;
+
+using Gerenciador_de_solucoes;
+
+namespace Gerenciador_de_solucoes
+{
+    public class ContaCorrente
+    {
+        //private Cliente _titular;
+
+        //public Cliente Titular
+        //{
+        //    get 
+        //    {
+        //        return _titular; 
+        //    }
+        //    set
+        //    {
+        //        _titular = value;
+        //    }
+        //}
+
+        public Cliente Titular{get;set;}
+
+        private int _agencia;
+        public int Agencia 
+        {
+            get
+            {
+                return _agencia;
+
+            }
+            set
+            {
+                if(value <= 0)
+                {
+                    return;
+                }
+
+            }
+        }
+        public int Numero { get;set;}
+
+
+        private double _saldo;
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                else
+                {
+                    _saldo = value;
+                }
+            }
+        }
+
+        public ContaCorrente(int agencia , int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (this._saldo < valor)
+            // this é usando para refrerencia que é desse objeto
+            {
+                return false;
+                // retorna o valor da função
+            }
+            else
+            {
+                this._saldo -= 100;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+    }
+}
+
---
+++ ex06/Construtures_membros_estaticos/Construtures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gerenciador_de_solucoes;
+
+namespace Construtores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(231, 8843854);
+
+        }
+    }
+}
